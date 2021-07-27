f8d25194beeab70252f265bc2877e55dad4db3dc
Kent <iamcxa@gmail.com>
2021-07-27 09:38:43 +0800

    fix: 🐛 export

--- api/controllers/SwaggerController.js
+++ api/controllers/SwaggerController.js
@@ -1,31 +1,32 @@
-
 const ejs = require('ejs');
 
-module.exports = async function ShowDoc(req, res) {
-  const {
-    project,
-  } = req.allParams();
-  try {
-    let swaggerObj = null;
-    if (project && sails.hooks[project]) {
-      swaggerObj = require(`${sails.config.appPath}/api/hooks/${project}/${sails.config['swagger-ui'].path}`);
-    } else {
-      swaggerObj = require(`${sails.config.appPath}/${sails.config['swagger-ui'].path}`);
-    }
+module.exports = {
+  ShowDoc: async function(req, res) {
+    const {
+      project,
+    } = req.allParams();
+    try {
+      let swaggerObj = null;
+      if (project && sails.hooks[project]) {
+        swaggerObj = require(`${sails.config.appPath}/api/hooks/${project}/${sails.config['swagger-ui'].path}`);
+      } else {
+        swaggerObj = require(`${sails.config.appPath}/${sails.config['swagger-ui'].path}`);
+      }
 
-    ejs.renderFile(
-      `${__dirname}/../../views/swagger-ui/index.ejs`,
-      {
-        data: swaggerObj,
-        version: sails.config['swagger-ui'].version,
-      },
-      {},
-      function (err, data) {
-        if (err) throw err;
-        return res.send(data);
-      },
-    );
-  } catch (e) {
-    return res.error(e);
+      ejs.renderFile(
+        `${__dirname}/../../views/swagger-ui/index.ejs`,
+        {
+          data: swaggerObj,
+          version: sails.config['swagger-ui'].version,
+        },
+        {},
+        function(err, data) {
+          if (err) throw err;
+          return res.send(data);
+        },
+      );
+    } catch (e) {
+      return res.error(e);
+    }
   }
 }
